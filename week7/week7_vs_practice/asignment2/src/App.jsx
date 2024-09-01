1db7455f7397bd650b3531707a034e58da022040
rekha jakhar <rekha_jakhar@intuit.com>
2024-09-01 15:39:48 +0530

    prop drilling

---
+++ week7/week7_vs_practice/asignment2/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+const [count, setCount] = useState(0);
+
+  return (
+    <div>
+     <Count count={count} setCount={setCount}/>
+    </div>
+  )
+}
+
+function Count({count, setCount}) {
+return <div>
+  <CountRenderer count={count}/>
+  <Buttons count={count} setCount={setCount}/>
+</div>
+}
+
+function CountRenderer({count}) {
+return <div>
+  {count}
+</div>
+}
+
+function Buttons({count, setCount}) {
+return <div>
+  <button onClick ={() => {
+setCount(count + 1)
+  }}>Increase</button>
+  <button onClick ={() => {
+setCount(count - 1)
+}}>Decrease</button>
+</div>
+}
+
+export default App
